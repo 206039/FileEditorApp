221670590817db4af7912b338c993d279ba7c0c3
Filip <206039@edu.p.lodz.pl>
2020-02-08 11:30:51 +0100

    Added Foreign Key to between DatbaseFile and User

--- FileEditorApp/Server/EF/AppContext.cs
+++ FileEditorApp/Server/EF/AppContext.cs
@@ -6,13 +6,18 @@ namespace FileEditorApp.Server.EF
     public class AppContext : DbContext
     {
         public DbSet<User> Users { get; private set; }
+        public DbSet<DatabaseFile> DatabaseFiles { get; private set; }
         public AppContext(DbContextOptions<AppContext> option) : base(option)
         {
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>()
+                .HasMany(x => x.DatabaseFiles)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
         }
     }
 }
