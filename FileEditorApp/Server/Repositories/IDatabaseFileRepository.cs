32b1b4a2b39903c83d67c0002e4d51dd98959de5
Filip <206039@edu.p.lodz.pl>
2020-02-08 11:57:05 +0100

    Added AddAsync to DatabaseFileRepository.

---
+++ FileEditorApp/Server/Repositories/IDatabaseFileRepository.cs
@@ -0,0 +1,10 @@
+﻿using FileEditorApp.Shared.Domain;
+using System.Threading.Tasks;
+
+namespace FileEditorApp.Server.Repositories
+{
+    public interface IDatabaseFileRepository : IRepository
+    {
+        Task AddAsync(DatabaseFile databaseFile);
+    }
+}
