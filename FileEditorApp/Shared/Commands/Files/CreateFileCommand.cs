b3c2dfeed4f99e9630c0c3217902dbef67769669
Filip <206039@edu.p.lodz.pl>
2020-02-08 13:02:43 +0100

    Added CreateFileCommand.

---
+++ FileEditorApp/Shared/Commands/Files/CreateFileCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace FileEditorApp.Shared.Commands.Files
+{
+    public class CreateFileCommand
+    {
+        public int UserId { get; set; }
+        public string Filename { get; private set; }
+    }
+}
