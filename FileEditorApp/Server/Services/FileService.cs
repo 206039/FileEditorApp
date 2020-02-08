50301f8869045fce1e8d6f4277266d93d69ec45a
Filip <206039@edu.p.lodz.pl>
2020-02-08 12:55:28 +0100

    Added CreateFileAsync in FileService.

---
+++ FileEditorApp/Server/Services/FileService.cs
@@ -0,0 +1,28 @@
+﻿using FileEditorApp.Server.Repositories;
+using FileEditorApp.Shared.Domain;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileEditorApp.Server.Services
+{
+    public class FileService : IFileService
+    {
+        private readonly IDatabaseFileRepository _dbFileRepository;
+        private readonly IFileRepository _fileRepository;
+
+        public FileService(IDatabaseFileRepository dbFileRepository, IFileRepository fileRepository)
+        {
+            _dbFileRepository = dbFileRepository;
+            _fileRepository = fileRepository;
+        }
+        public async Task CreateFileAsync(int userId, string fileName)
+        {
+            var uriFileName = Guid.NewGuid().ToString();
+            var uri = Path.Combine("Files", uriFileName);
+            var dbFile = new DatabaseFile(userId, fileName, uri);
+            await _dbFileRepository.AddAsync(dbFile);
+            _fileRepository.CreateFile(dbFile.Uri);
+        }
+    }
+}
---
+++ FileEditorApp/Server/Services/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace FileEditorApp.Server.Services
+{
+    public interface IFileService : IService
+    {
+        Task CreateFileAsync(int userId, string fileName);
+    }
+}
